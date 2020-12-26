86b857f3c0c0884e28082bb6353b93c63b8e5b46
root <root@6b5c0cc4711c.mylabserver.com>
2020-12-26 16:20:08 +0000

    terraform

---
+++ ec2.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+data "aws_ami" "ec2-ami" {
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["Packer-Ansible"]
+  }
+  most_recent = true
+  owners = ["self"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ec2-ami.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "automation"
+  }
+}
