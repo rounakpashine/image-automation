08d5731132eb908b454268c7b276fad35f1d3622
Shweta Kumari <69496909+shweta-github04@users.noreply.github.com>
2020-12-27 21:18:39 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -23,28 +23,3 @@ resource "aws_instance" "web" {
     Name = "automation"
   }
 }
-  
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = "vpc-04a19ec0df5243ed0"
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }  
-}
