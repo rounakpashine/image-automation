efa24c6ef493ed7b6c52f55e07788622ea5914f2
Shweta Kumari <69496909+shweta-github04@users.noreply.github.com>
2020-12-27 21:10:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -17,9 +17,34 @@ data "aws_ami" "ec2-ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ec2-ami.id
-  instance_type = "t1.medium"
+  instance_type = "t3.medium"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "automation"
   }
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-04a19ec0df5243ed0"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}  
 }
