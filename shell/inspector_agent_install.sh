3878fa9cc992f19e53950b0e6e86cc664ee99fe7
Rounak <rounakpashine.sysops@gmail.com>
2020-12-31 08:02:41 +0000

    add inspector

---
+++ shell/inspector_agent_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "*******Installing AWS Inspector Agent********"
+curl -o "/tmp/inspector_install" https://inspector-agent.amazonaws.com/linux/latest/install
+
+sudo bash /tmp/inspector_install
+
+sudo /etc/init.d/awsagent start
+
+rm -rf /tmp/inspector_install
